a53aa16133b0179f21d642ca033789421ce66437
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-03-25 15:15:34 +0530

    Time: 50 ms (98.50%), Space: 14 MB (32.10%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, cand: List[int], tar: int) -> List[List[int]]:
+        
+        ans=[]
+        cand.sort()
+        def solve(idx,tr,temp):
+            
+            
+            if tr==0:
+                ans.append(temp.copy())
+                return
+            
+            
+            if idx>=len(cand) or cand[idx]>tr:
+                return
+            
+            # for i in range(idx,len(can)):
+            #     temp.append(can[i])
+            #     solve(can,i,tr-can[i],temp)
+            #     temp.pop()
+            solve(idx+1,tr,temp)
+            temp.append(cand[idx])
+            solve(idx,tr-cand[idx],temp)
+            temp.pop()
+        solve(0,tar,[])
+        return ans
+    
+            
+        
