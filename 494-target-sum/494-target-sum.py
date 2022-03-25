916071b03005a25783c4eaf50708dfa5627da0ad
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-03-25 16:02:36 +0530

    Time: 468 ms (43.19%), Space: 35.9 MB (37.87%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], tar: int) -> int:
+        dp=dict()
+        def solve(idx,csum):
+            if csum==tar and idx==len(nums):
+                return 1
+            if idx==len(nums):
+                return 0
+            
+            if (csum,idx) in dp.keys():
+                return dp[(csum,idx)]
+            
+            dp[(csum,idx)]=solve(idx+1,csum-nums[idx])+solve(idx+1,csum+nums[idx])
+            return dp[(csum,idx)]
+                
+        return solve(0,0)      
+        
