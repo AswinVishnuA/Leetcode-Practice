11d82da6201189e4081e5bc6551cdd4620088675
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-06-23 16:26:39 +0530

    Time: 255 ms (22.19%), Space: 23.5 MB (5.21%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        
+        s=set()
+        def dfs(a,t):
+            if a:
+                s.add(a)
+                                        
+            for i in range(len(t)):
+                dfs(a+t[i],t[:i]+t[i+1:])
+        
+        dfs("",tiles)
+        return len(s)
+                
+        
