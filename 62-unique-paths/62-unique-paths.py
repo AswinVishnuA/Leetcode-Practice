4b88cd139253ab7ae84417cf62a44bf3f44a5192
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-08-01 08:50:52 +0530

    Time: 40 ms (76.12%), Space: 14.4 MB (6.77%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        
+        @lru_cache(None)
+        def solve(i,j):
+            
+            if i==m or j==n or i<0 or j<0:
+                return 0
+            
+            
+            if i==m-1 and j==n-1:
+                return 1
+            
+            return solve(i+1,j)+solve(i,j+1)
+        
+        return solve(0,0)
+        
