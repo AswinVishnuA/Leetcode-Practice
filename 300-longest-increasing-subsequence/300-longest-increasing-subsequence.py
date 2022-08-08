7757eb33b87fc1d3102278cdff3e52ff0c6dd9c3
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-08-08 10:32:18 +0530

    Time: 82 ms (96.12%), Space: 14.1 MB (83.70%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -3,36 +3,58 @@ class Solution:
         
         n=len(nums)
         
-#         dp=[[-1 for i in range(20002)] for _ in range(2501)]
+#         dp=[[-1 for i in range(2502)] for _ in range(2501)]
         
-#         def solve(i,prev=-10001):
+#         def solve(i,prev=-1):
 #             # print(i,prev)
 #             if i==n:
 #                 return 0
             
-#             if dp[i][prev+10001]!=-1:
-#                 return dp[i][prev+10001]
+#             if dp[i][prev+1]!=-1:
+#                 return dp[i][prev+1]
             
-#             dp[i][prev+10001]=solve(i+1,prev)
+#             dp[i][prev+1]=solve(i+1,prev)
             
-#             if nums[i]>prev:
-#                 dp[i][prev+10001]=max(dp[i][prev+10001],1+solve(i+1,nums[i]))
+#             if prev==-1 or nums[i]>nums[prev]:
+#                 dp[i][prev+1]=max(dp[i][prev+1],1+solve(i+1,i))
             
             
-#             return dp[i][prev+10001]        
+#             return dp[i][prev+1]        
         
 #         return solve(0)
 
 
-        dp=[1 for i in range(n)]
+#         dp=[1 for i in range(n)]
             
         
-        for i in range(n):
+#         for i in range(n):
             
-            for j in range(i):
-                # print(dp,i,j)
+#             for j in range(i):
+#                 # print(dp,i,j)
 
-                if nums[i]>nums[j] and dp[i]<dp[j]+1:
-                    dp[i]=dp[j]+1
-        return max(dp)
+#                 if nums[i]>nums[j] and dp[i]<dp[j]+1:
+#                     dp[i]=dp[j]+1
+#         return max(dp)
+        dp=[nums[0]]
+        
+        for i in range(1,n):
+            
+            idx = bisect_right(dp,nums[i])
+            
+            if idx==len(dp):
+                if nums[i]==dp[idx-1]:
+                    continue
+                else:
+                    dp.append(nums[i])
+            else:
+                if nums[i]==dp[idx-1]:
+                    continue
+                else:
+                    dp[idx]=nums[i]
+        
+        return len(dp)
+                
+            
+            
+            
         
