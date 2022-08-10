cca41a6ae5f612c15340bbe3685a18ba4388f798
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-08-10 11:58:15 +0530

    Time: 600 ms (64.05%), Space: 66.1 MB (62.25%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -5,63 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        
-        ans=[0]
-        
-#         @lru_cache(None)
-#         def isbst(cur,leftl,rightl):
-            
-#             if not cur:
-#                 return True
-            
-#             if leftl<cur.val<rightl:
-                
-#                 return isbst(cur.left,leftl,cur.val) and isbst(cur.right,cur.val,rightl) 
-            
-#             return False
-        
-#         @lru_cache(None)
-#         def findsum(cur):
-            
-#             if not cur:
-#                 return 0
-            
-#             return cur.val + findsum(cur.left) + findsum(cur.right)
-        
-        @lru_cache(None)
-        def checkandsum(cur,leftl,rightl):
-            
-            if not cur:
-                return True,0
-            
-            if leftl<cur.val<rightl:
-                leftb,sumvall=checkandsum(cur.left,leftl,cur.val)
-                rightb,sumvalr=checkandsum(cur.right,cur.val,rightl)
-                
-                if leftb and rightb:
-                    return True,cur.val+sumvall+sumvalr
-            
-            return False,0
-                    
-                
-            
-            return False
-        
-        def dfs(cur):
-            
-            if cur:
-                
-                check,sumval=checkandsum(cur,float("-inf"),float("inf"))
-                
-                if check:
-                    ans[0]=max(ans[0],sumval)
-                
-                
-                dfs(cur.left)
-                dfs(cur.right)
+    def maxSumBST(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        self.s = 0
+        def helper(cur):
+            a, amin, amax = helper(cur.left) if cur.left else (0, float('inf'), float('-inf'))
+            b, bmin, bmax = helper(cur.right) if cur.right else (0, float('inf'), float('-inf'))
+            if a is not None and b is not None:
+                if cur.val > amax and cur.val < bmin:
+                    s = cur.val + a + b
+                    self.s = max(self.s, s)
+                    # print(cur.val,a,b,s,min(cur.val, amin), max(cur.val, bmax))
 
-        
-        dfs(root)
-        
-        return ans[0]
+                    return s, min(cur.val, amin), max(cur.val, bmax)
+
+            # print(cur.val,a,b)
+            return (None, None, None)
+        helper(root)
+        return self.s
