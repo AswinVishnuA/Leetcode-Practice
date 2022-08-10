44c3c392a1b48ebe444b92cbc2deedf937a698d4
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-08-10 11:11:06 +0530

    Time: 1673 ms (5.06%), Space: 100.3 MB (5.72%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -9,46 +9,58 @@ class Solution:
         
         ans=[0]
         
+#         @lru_cache(None)
+#         def isbst(cur,leftl,rightl):
+            
+#             if not cur:
+#                 return True
+            
+#             if leftl<cur.val<rightl:
+                
+#                 return isbst(cur.left,leftl,cur.val) and isbst(cur.right,cur.val,rightl) 
+            
+#             return False
+        
+#         @lru_cache(None)
+#         def findsum(cur):
+            
+#             if not cur:
+#                 return 0
+            
+#             return cur.val + findsum(cur.left) + findsum(cur.right)
+        
         @lru_cache(None)
-        def isbst(cur,leftl,rightl):
+        def checkandsum(cur,leftl,rightl):
             
             if not cur:
-                return True
+                return True,0
             
             if leftl<cur.val<rightl:
+                leftb,sumvall=checkandsum(cur.left,leftl,cur.val)
+                rightb,sumvalr=checkandsum(cur.right,cur.val,rightl)
                 
-                return isbst(cur.left,leftl,cur.val) and isbst(cur.right,cur.val,rightl) 
+                if leftb and rightb:
+                    return True,cur.val+sumvall+sumvalr
+            
+            return False,0
+                    
+                
             
             return False
-        
-        @lru_cache(None)
-        def findsum(cur):
-            
-            if not cur:
-                return 0
-            
-            return cur.val + findsum(cur.left) + findsum(cur.right)
-        
         
         def dfs(cur):
             
             if cur:
                 
-                if isbst(cur,float("-inf"),float("inf")):
-                    
-                    ans[0]=max(ans[0],findsum(cur))
+                check,sumval=checkandsum(cur,float("-inf"),float("inf"))
+                
+                if check:
+                    ans[0]=max(ans[0],sumval)
                 
                 
                 dfs(cur.left)
                 dfs(cur.right)
 
-                    
-                    
-            
-            
-            
-        
-        
         
         dfs(root)
         
