c82290300490155608e71637aa41da58c1c3eea0
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-08-27 13:21:14 +0530

    Time: 156 ms (60.20%), Space: 14.7 MB (36.91%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -4,9 +4,9 @@ class Solution:
         needed=Counter(t)
         missing=len(t)
         
-        start,end=0,0
+        start,end=0,-1
         i=0
-        for j,char in enumerate(s,1):
+        for j,char in enumerate(s):
             
             if needed[char]>0:
                 missing-=1
@@ -22,11 +22,11 @@ class Solution:
                 missing+=1
                 needed[s[i]]+=1
                 
-                if end==0 or j-i<end-start:
+                if end==-1 or j-i<end-start:
                     start,end=i,j
                 i+=1
         
-        return s[start:end]
+        return s[start:end+1]
                 
             
             
