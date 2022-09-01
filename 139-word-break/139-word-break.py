01ef5c47b514f5fecdd78cff3039d398c45c9fc7
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-01 21:45:47 +0530

    Time: 45 ms (84.66%), Space: 14.9 MB (6.11%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -3,19 +3,30 @@ class Solution:
         
         wordSet=set(wordDict)
         
-        @lru_cache(None)
-        def solve(string):
+        trueSet=set()
+        
+        dp=[[-1]*(len(s)+1) for _ in range(len(s)+1)]
+        
+        def solve(i,j):
             
             
-            if string=="" or string in wordSet:
+            if s[i:j]=="":
                 return True
-            # print(string)
-            for i in range(1,len(string)):
-                if string[:i] in wordSet and  solve(string[i:]):
+            
+            # print(i,j)
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
+            for k in range(i+1,j+1):
+                if s[i:k] in wordSet and  solve(k,j):
+                    dp[i][j]= 1
                     return True
+            
+            dp[i][j]= 0
             
             return False
         
-        return solve(s)
+        # print(dp)
+        return solve(0,len(s))
         
         
