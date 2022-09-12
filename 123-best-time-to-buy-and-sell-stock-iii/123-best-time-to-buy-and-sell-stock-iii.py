220c515edb80d09e726344c351463e0c04f95ec2
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-12 23:54:19 +0530

    Time: 4056 ms (21.76%), Space: 36.5 MB (30.30%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,20 +1,34 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        k=2
+        
         n=len(prices)
+        dp=[[0 for i in range(3)] for j in range(n)]
         
-        dp=[[0 for i in range(n+1)] for _ in range(k+1)]
-        
-        if n<1:
-            return 0
-        
-        for i in range(1,k+1):
+        ans=0
+        for i in range(1,3):
             maxval=-prices[0]
             for j in range(1,n):
                 
-                dp[i][j]=max(dp[i][j-1],prices[j]+maxval)
-                maxval=max(maxval,dp[i-1][j-1]-prices[j])
+                dp[j][i]=max(dp[j-1][i],prices[j]+maxval)
+                ans=max(ans,dp[j][i])
+                maxval=max(dp[j-1][i-1]-prices[j],maxval)
+        
+        # print(dp)
+        return ans
+#         n=len(prices)
+        
+#         dp=[[0 for i in range(n+1)] for _ in range(k+1)]
+        
+#         if n<1:
+#             return 0
+        
+#         for i in range(1,k+1):
+#             maxval=-prices[0]
+#             for j in range(1,n):
+                
+#                 dp[i][j]=max(dp[i][j-1],prices[j]+maxval)
+#                 maxval=max(maxval,dp[i-1][j-1]-prices[j])
                 
         
-        return dp[k][n-1]
+#         return dp[k][n-1]
