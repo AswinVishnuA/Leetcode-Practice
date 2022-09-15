213ddac62464c9dfc4e3c433bb2d90bb5b946d1d
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-15 20:55:38 +0530

    Time: 95 ms (21.24%), Space: 17.1 MB (45.53%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,22 +4,28 @@ class Solution:
         
         n=len(prices)
         
-        @lru_cache(None)
+        dp=[[-1,-1] for _ in range(n+1)]
+        
+        
         def solve(i,toSell):
             
             if i>=n:
                 return 0
             
+            if dp[i][toSell]!=-1:
+                return dp[i][toSell]
+            
             if toSell:
                 
                 sell=solve(i+2,False)+prices[i]
                 noSell=solve(i+1,True)
-                return max(sell,noSell)
+                dp[i][toSell]= max(sell,noSell)
+                return dp[i][toSell]
             else:
                 buy=solve(i+1,True)-prices[i]
                 noBuy=solve(i+1,False)
+                dp[i][toSell]=max(buy,noBuy)
+                return dp[i][toSell]
             
-                return max(buy,noBuy)
-        
         return solve(0,False)
         
