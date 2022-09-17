a02af3e5af40bbd6045009a716226fd2e8a74b53
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-17 15:54:14 +0530

    Time: 816 ms (36.31%), Space: 14 MB (91.24%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -19,21 +19,20 @@ class Solution:
         
 #         return solve(n,amount)
         
-        dp=[[0]*(amount+1) for i in range(n+1)]
+        dp=[0]*(amount+1)
         
-        for i in range(n+1):
-            dp[i][0]=1
+        dp[0]=1
         
         
         for i in range(1,n+1):
             for tar in range(1,amount+1):
                 
                 if coins[i-1]>tar:
-                    dp[i][tar]=dp[i-1][tar]
+                    continue
                 else:
-                    dp[i][tar]=dp[i-1][tar]+dp[i][tar-coins[i-1]]
+                    dp[tar]+=dp[tar-coins[i-1]]
         
-        return dp[n][amount]
+        return dp[amount]
                 
             
         
