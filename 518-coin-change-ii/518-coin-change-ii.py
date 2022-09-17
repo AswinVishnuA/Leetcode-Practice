9fc60d345de4f3fdf30206865a59578b66183875
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-17 15:51:11 +0530

    Time: 963 ms (30.23%), Space: 29.4 MB (43.85%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -3,20 +3,37 @@ class Solution:
         
         n=len(coins)
         # coins.sort()
-        @lru_cache(None)
-        def solve(i,tar):
+#         @lru_cache(None)
+#         def solve(i,tar):
             
-            if tar==0:
-                return 1
+#             if tar==0:
+#                 return 1
             
-            if i==0:
-                return 0
+#             if i==0:
+#                 return 0
             
-            if coins[i-1]>tar:
-                return solve(i-1,tar)
+#             if coins[i-1]>tar:
+#                 return solve(i-1,tar)
             
-            return solve(i,tar-coins[i-1]) + solve(i-1,tar)
+#             return solve(i,tar-coins[i-1]) + solve(i-1,tar)
         
-        return solve(n,amount)
+#         return solve(n,amount)
+        
+        dp=[[0]*(amount+1) for i in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0]=1
+        
+        
+        for i in range(1,n+1):
+            for tar in range(1,amount+1):
+                
+                if coins[i-1]>tar:
+                    dp[i][tar]=dp[i-1][tar]
+                else:
+                    dp[i][tar]=dp[i-1][tar]+dp[i][tar-coins[i-1]]
+        
+        return dp[n][amount]
+                
             
         
