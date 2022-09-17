6efc4751c7da486b0760e7edd1b65af07c055091
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2022-09-17 11:58:27 +0530

    Time: 1535 ms (15.21%), Space: 22 MB (79.19%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,24 +1,41 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        len1=len(text1)
-        len2=len(text2)
+#         len1=len(text1)
+#         len2=len(text2)
         
-        @lru_cache(None)
-        def solve(m,n):
+#         @lru_cache(None)
+#         def solve(m,n):
             
-            if m==0 or n==0:
-                return 0
+#             if m==0 or n==0:
+#                 return 0
             
-            ans=0
+#             ans=0
             
-            if text1[m-1]==text2[n-1]:
-                ans=1+solve(m-1,n-1)
+#             if text1[m-1]==text2[n-1]:
+#                 ans=1+solve(m-1,n-1)
             
-            ans=max(ans,solve(m-1,n),solve(m,n-1))
+#             ans=max(ans,solve(m-1,n),solve(m,n-1))
             
-            return ans
+#             return ans
         
         
-        return solve(len1,len2)
+#         return solve(len1,len2)
+        
+        m=len(text1)
+        n=len(text2)
+        
+        dp=[[0]*(n+1) for i in range(m+1)]
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                
+                dp[i][j]=max(dp[i][j],dp[i-1][j],dp[i][j-1])
+        
+        return dp[m][n]
+                    
+        
         
